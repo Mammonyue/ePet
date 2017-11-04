9bcce65d99a6cc6f1ea727e859d1179450ec5b95
Mammonyue <519442140@qq.com>
2017-11-04 14:01:01 +0800

    thi

--- src/components/content/content.vue
+++ src/components/content/content.vue
@@ -5,16 +5,56 @@
           <li>
             <p>11.1-11.8</p>
             <span>·三折起预售·</span>
+            <div class="mask"> > </div>
           </li>
           <li>
             <p>11.9-11.11</p>
             <span>1折起狂欢·折上再9折</span>
+            <div  class="mask"> > </div>
           </li>
           <li>
             <p>11.12-11.19</p>
             <span>享全年折上8.8折</span>
           </li>
         </ul>
+    </div>
+    <div class="banner">
+      <div id="ban_wrap">
+        <ul id="ban_list">
+          <li>
+            <a href="###">
+              <img src="img/1.jpg"/>
+            </a>
+          </li>
+          <li>
+            <a href="###">
+              <img src="img/2.jpg"/>
+            </a>
+          </li>
+          <li>
+            <a href="###">
+              <img src="img/3.jpg"/>
+            </a>
+          </li>
+          <li>
+            <a href="###">
+              <img src="img/4.jpg"/>
+            </a>
+          </li>
+          <li>
+            <a href="###">
+              <img src="img/5.jpg"/>
+            </a>
+          </li>
+        </ul>
+        <div id="ban_navs">
+          <span class="active"></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
